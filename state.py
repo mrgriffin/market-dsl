f3c4770ea60e8668060f0fbd826d05e55b127f99
Martin Griffin <martinrgriffin@gmail.com>
2014-12-26 20:22:22 +0000

    make structures instantiatable

--- state.py
+++ state.py
@@ -1,7 +1,35 @@
 from collections import namedtuple
 
+
+class Period(object):
+    def __init__(self, incidents, open):
+        self.incidents = incidents
+        self.open = open
+
+    def __repr__(self):
+        return repr((self.incidents, self.open))
+
+
+def parse_state(structure, incidents):
+    # TODO: Verify that incidents matches the structure.
+    state = []
+    for i in incidents:
+        # TODO: Support nested periods.  Requires period-begin to carry
+        #       a depth?
+        if i is period.begin:
+            state.append(Period([], True))
+        elif i is period.end:
+            state[-1].open = False
+        else:
+            state[-1].incidents.append(i)
+    return state
+
 structure = namedtuple('structure', ['periods'])
+structure.__call__ = parse_state
+
 period = namedtuple('period', ['exit', 'incidents'])
+period.begin = 'PBEG'
+period.end = 'PEND'
 
 time = 'time'
 goal = namedtuple('goal', ['team'])
@@ -9,3 +37,9 @@ goal = namedtuple('goal', ['team'])
 football = structure((period(exit=time, incidents=(goal,)),) * 2)
 
 print football
+
+incidents = [period.begin, goal('home'), period.end, period.begin]
+
+state = football(incidents)
+
+print state
