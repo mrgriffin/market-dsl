68800a7854f9081a8eda8166dde6395be2633978
Martin Griffin <martinrgriffin@gmail.com>
2014-12-28 06:37:31 +0000

    add expected price trends

--- market.py
+++ market.py
@@ -1,7 +1,9 @@
 import __builtin__  # Unneeded once eval is separated.
 from collections import namedtuple
+from itertools import chain, combinations
 from operator import attrgetter
 
+from toolz.dicttoolz import valmap
 from toolz.itertoolz import groupby
 from singledispatch import singledispatch
 
@@ -71,8 +73,21 @@ def typeof_str(expr):
     return expr
 
 
+@singledispatch
 def members(type, env):
-    """Returns the members of a type."""
+    raise NotImplementedError
+
+
+# TODO: This is the type of what max returns, which isn't quite a set.
+@members.register(set)
+def members_set(type, env):
+    values = members(next(iter(type)), env)
+    combs = (combinations(values, i) for i in xrange(1, len(values) + 1))
+    return list(chain.from_iterable(combs))
+
+
+@members.register(str)
+def members_str(type, env):
     return globals()['members_' + type](env)
 
 
@@ -80,6 +95,46 @@ def members_team(env):
     return env.teams
 
 
+@singledispatch
+def rank(expr, env):
+    raise NotImplementedError
+
+
+@rank.register(max)
+def rank_max(expr, env):
+    coll = eval(expr.coll, env)
+
+    def key((k, v)): return expr.key(v)
+    groups = groupby(key, coll.iteritems())
+    max = __builtin__.max(groups)
+    unique_max = __builtin__.max(groups) + (len(groups[max]) != 1)
+
+    keys = members(typeof(expr), env)
+    values = valmap(expr.key, coll)
+
+    def maxdiff(ks):
+        try:
+            return __builtin__.max(abs(values[a] - values[b])
+                                   for a, b in combinations(ks, 2))
+        except ValueError:
+            if values[ks[0]] == unique_max:
+                return 0
+            else:
+                # TODO: How does this math change for sports with non-1
+                #       scores (e.g. rugby)?
+                return 1 + max - values[ks[0]]
+
+    return {ks: maxdiff(ks) for ks in keys}
+
+
+def trend(expr, env0, env1):
+    rank0 = rank(expr, env0)
+    rank1 = rank(expr, env1)
+    return {k: cmp(rank0[k], rank1[k]) for k in rank0}
+
+
 print eval(most_goals, football_state)
 print eval(most_goals, football_nil)
 print typeof(most_goals)
+print trend(most_goals, football_nil, football_state)
+print trend(most_goals, football_nil, football_nil)
